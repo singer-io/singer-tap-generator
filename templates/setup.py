641b2ff711e0932e4f2f76350a64a7a51a538e9a
Rutuja Deshmukh <107538720+rdeshmukh15@users.noreply.github.com>
2025-08-13 11:33:02 +0530

    Update setup.py

--- templates/setup.py
+++ templates/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name="tap-{{ config.tap_name|lower }}",
-      version="1.0.0",
+      version="0.0.1",
       description="Singer.io tap for extracting data from {{ config.tap_name }} API",
       author="{{config.author if config.author else Stitch}}",
       url="http://singer.io",
